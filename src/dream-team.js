075142c54f03ca2f9f630d751381368c3a6d5544
Aleksandr <sunseticy@gmail.com>
2020-10-07 21:25:32 +0400

    feat: close task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = '';
+  if (members == null || members.constructor != Array) return false;
+  for (let i of members) {
+    if (typeof i === 'string') {
+      let name = i.trim().toUpperCase();
+      result += name[0];
+    }
+  }
+  return result.split('').sort().join('');
 };
